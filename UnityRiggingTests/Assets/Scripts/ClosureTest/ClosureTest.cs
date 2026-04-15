4956d418794c4c74c33c4828f528f2a43b51e6a8
Povet34 <70003696+Povet34@users.noreply.github.com>
2026-04-15 21:59:39 +0900

    Add IDE project files & update ClosureTest

--- UnityRiggingTests/Assets/Scripts/ClosureTest/ClosureTest.cs
+++ UnityRiggingTests/Assets/Scripts/ClosureTest/ClosureTest.cs
@@ -7,13 +7,7 @@ public class ClosureTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var refInt = new RefValue<int>(1);
     }
 }
 
